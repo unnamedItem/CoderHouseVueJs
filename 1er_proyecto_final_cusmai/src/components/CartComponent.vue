ada71b1eb3a9a30a9cc7e488c30550f313ad6aca
fcusmai <fcusmai@b2gov.com>
2023-06-15 17:37:49 -0300

    first commit

---
+++ 1er_proyecto_final_cusmai/src/components/CartComponent.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <div v-if="cartList.length > 0">
+            <b-row v-for="(product, index) in cartList" :key="index" class="cart-list">
+                <b-col cols="12"><em><b>{{ product.name }}</b></em></b-col>
+                <b-col cols="4">
+                    <div class="product-img">
+                        <img :src="product.img" :alt="product.name" width="100" height="100" class="pr-2 pb-2">
+                    </div>
+                </b-col>
+                <b-col cols="8" class="my-auto">
+                    <b-input-group>
+                        <b-input-group-prepend>
+                            <b-button sm variant="outline-danger" @click="subItem(product.id)">-</b-button>
+                        </b-input-group-prepend>
+
+                        <b-form-input sm disabled v-model="product.quantity" type="number" min="0"></b-form-input>
+
+                        <b-input-group-append>
+                            <b-button sm variant="outline-success" @click="addItem(product.id)">+</b-button>
+                        </b-input-group-append>
+
+                        <b-input-group-append>
+                            <b-button sm variant="outline-danger" @click="removeItem(product.id)"><b-icon icon="trash"></b-icon></b-button>
+                        </b-input-group-append>
+                    </b-input-group>
+                </b-col>
+            </b-row>
+
+            <b-button block variant="danger">Finalizar compra</b-button>
+        </div>
+        <div v-else>
+            <b-alert show variant="light" style="width:250px">
+                <span>El carrito esta vacío</span>
+            </b-alert>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "CartComponent",
+    props: {
+        cartList: Array
+    },
+    methods: {
+        addItem(id) {
+            this.$emit("addItem", id);
+        },
+        subItem(id) {
+            this.$emit("subItem", id);
+        },
+        removeItem(id) {
+            this.$emit("removeItem", id);
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+.cart-list {
+    min-width: 300px;
+    border: 1px solid lightgray;
+    border-radius: 10px;
+    margin: 10px;
+}
+</style>
---
+++ 1er_proyecto_final_cusmai/src/components/ProductCardInfo.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <p>{{ product.desc }}</p>
+        <b-table small stacked :items="[product]" :fields="fields"></b-table>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "ProductCardInfo",
+    props: {
+        product: Object
+    },
+    data() {
+        return {
+            fields: [
+                {
+                    key: 'price',
+                    label: 'Precio',
+                    sortable: false,
+                    variant: "secondary",
+                    formatter: (val) => `$${val.toFixed(2)}`
+                },
+                {
+                    key: 'maxPlayers',
+                    label: 'Max. Jugadores',
+                    sortable: false,
+                },
+                {
+                    key: 'minPlayers',
+                    label: 'Min. Jugadores',
+                    sortable: false,
+                    variant: "secondary",
+                },
+                {
+                    key: 'gameTime',
+                    label: 'Duración',
+                    sortable: false,
+                    formatter: (val) => `${val} minutos`
+                },
+            ]
+        }
+    }
+}
+</script>
