0c72559de7d0a6929abea0dd71acbbb2e3218f27
fcusmai <fcusmai@b2gov.com>
2023-07-31 21:19:52 -0300

    product filter

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,70 +1,99 @@
 <template>
-    <b-navbar toggleable="lg" type="dark" variant="danger">
-        <b-navbar-brand to="/">Tienda de juegos</b-navbar-brand>
+  <b-navbar toggleable="lg" type="dark" variant="danger">
+    <b-navbar-brand to="/">Tienda de juegos</b-navbar-brand>
 
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-        <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav>
-                <b-nav-item to="/">Clasicos</b-nav-item>
-                <b-nav-item to="/">Euros</b-nav-item>
-                <b-nav-item to="/">Party Games</b-nav-item>
-            </b-navbar-nav>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item :to="{ name: 'Home', query: { tag: 'euro' } }"
+          >Euros</b-nav-item
+        >
+        <b-nav-item :to="{ name: 'Home', query: { tag: 'party' } }"
+          >Party Games</b-nav-item
+        >
+        <b-nav-item :to="{ name: 'Home', query: { tag: 'co-op' } }"
+          >Co-Op</b-nav-item
+        >
+      </b-navbar-nav>
 
-            <b-navbar-nav class="ml-auto">
-                <b-nav-form>
-                    <b-form-input size="sm" class="mr-sm-2" placeholder="Buscar"></b-form-input>
-                    <b-button size="sm" class="my-2 my-sm-0" variant="light" type="submit"><b-icon
-                            icon="search"></b-icon></b-button>
-                </b-nav-form>
+      <b-navbar-nav class="ml-auto" @submit.prevent="handleSearch">
+        <b-nav-form>
+          <b-form-input
+            size="sm"
+            class="mr-sm-2"
+            v-model="searchQuery"
+            placeholder="Buscar"
+          ></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" variant="light" type="submit"
+            ><b-icon icon="search"></b-icon
+          ></b-button>
+        </b-nav-form>
 
-                <b-nav-item-dropdown right v-if="logged">
-                    <template #button-content>
-                        <em>Cuenta</em> <b-icon icon="person"></b-icon>
-                    </template>
-                    <b-dropdown-item v-if="hasAdminRoles" to="/productmanagement">Gestión Productos</b-dropdown-item>
-                    <b-dropdown-item v-if="hasAdminRoles" to="/usermanagement">Gestión Usuarios</b-dropdown-item>
-                    <b-dropdown-item @click="logout">Salir</b-dropdown-item>
-                </b-nav-item-dropdown>
+        <b-nav-item-dropdown right v-if="logged">
+          <template #button-content>
+            <em>Cuenta: {{ user.username }}</em> <b-icon icon="person"></b-icon>
+          </template>
+          <b-dropdown-item v-if="hasAdminRoles" to="/productmanagement"
+            >Gestión Productos</b-dropdown-item
+          >
+          <b-dropdown-item v-if="hasAdminRoles" to="/usermanagement"
+            >Gestión Usuarios</b-dropdown-item
+          >
+          <b-dropdown-item @click="logout">Salir</b-dropdown-item>
+        </b-nav-item-dropdown>
 
-                <b-nav-item right to="/login" v-else>
-                    <em>Ingresar</em> <b-icon icon="box-arrow-in-right"></b-icon>
-                </b-nav-item>
+        <b-nav-item right to="/login" v-else>
+          <em>Ingresar</em> <b-icon icon="box-arrow-in-right"></b-icon>
+        </b-nav-item>
 
-                <b-nav-item right to="/cart">
-                    <em>Carrito</em> <b-icon icon="cart"></b-icon> <b-badge pill variant="dark" v-show="cartQuantity">{{
-                        cartQuantity }}</b-badge>
-                </b-nav-item>
-            </b-navbar-nav>
-        </b-collapse>
-    </b-navbar>
+        <b-nav-item right to="/cart">
+          <em>Carrito</em> <b-icon icon="cart"></b-icon>
+          <b-badge pill variant="dark" v-show="cartQuantity">{{
+            cartQuantity
+          }}</b-badge>
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 
 <script>
-import { mapActions, mapState } from 'vuex';
+import { mapActions, mapState } from "vuex";
 const ADMIN = "admin";
 
 export default {
-    name: "TheNavbar",
-    computed: {
-        ...mapState({
-            cartList: state => state.cartModule.cart,
-            user: state => state.accountModule.user,
-            logged: state => state.accountModule.logged,
-        }),
-        cartQuantity() {
-            return this.cartList.reduce((total, product) => total + product.quantity, 0);
-        },
-        hasAdminRoles() {
-            return this.user?.roles?.some(rol => rol === ADMIN);
-        }
+  name: "TheNavbar",
+  data() {
+    return {
+      searchQuery: "",
+    };
+  },
+  computed: {
+    ...mapState({
+      cartList: (state) => state.cartModule.cart,
+      user: (state) => state.accountModule.user,
+      logged: (state) => state.accountModule.logged,
+    }),
+    cartQuantity() {
+      return this.cartList.reduce(
+        (total, product) => total + product.quantity,
+        0
+      );
     },
-    methods: {
-        ...mapActions("accountModule", ["logout"]),
-        login() {
-            this.$router.push("login");
-        }
-    }
-}
+    hasAdminRoles() {
+      return this.user?.roles?.some((rol) => rol === ADMIN);
+    },
+  },
+  methods: {
+    ...mapActions("accountModule", ["logout"]),
+    login() {
+      this.$router.push("login");
+    },
+    handleSearch() {
+      this.$router.push({ name: "Home", query: { search: this.searchQuery } });
+    },
+  },
+};
 </script>
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,39 +1,55 @@
 <template>
-    <b-overlay :show="!products.length">
-        <b-row class="grid">
-            <b-col cols="12" sm="6" md="4" lg="3" v-for="(product, index) in products" :key="index">
-                <ProductCard :product="product"/>
-            </b-col>
-        </b-row>
-    </b-overlay>
+  <b-overlay :show="!products.length">
+    <b-row class="grid">
+      <b-col
+        cols="12"
+        sm="6"
+        md="4"
+        lg="3"
+        v-for="(product, index) in productsFiltered"
+        :key="index"
+      >
+        <ProductCard :product="product" />
+      </b-col>
+    </b-row>
+  </b-overlay>
 </template>
 
 
 <script>
-import { mapState } from 'vuex';
-import ProductCard from './ProductCard.vue'
+import { mapState } from "vuex";
+import ProductCard from "./ProductCard.vue";
 
 export default {
-    name: "ProductList",
-    components: {
-        ProductCard,
+  name: "ProductList",
+  components: {
+    ProductCard,
+  },
+  data() {
+    return {
+      isLoading: true,
+    };
+  },
+  computed: {
+    ...mapState({
+      products: (state) => state.productModule.products,
+    }),
+    productsFiltered() {
+      const tag = this.$route.query.tag;
+      const searchQuery = this.$route.query.search;
+      return this.products.filter((prod) => {
+        const matchTag = prod.tags.includes(tag) || !tag;
+        const matchSearchQuery = prod.name.toLowerCase().includes(searchQuery) || !searchQuery;
+        return matchTag && matchSearchQuery;
+      });
     },
-    data() {
-        return {
-            isLoading: true
-        }
-    },
-    computed: {
-        ...mapState({
-            products: state => state.productModule.products,
-        }),
-    }
-}
+  },
+};
 </script>
 
 <style scoped>
 .grid {
-    margin-left: 5%;
-    margin-right: 5%;
+  margin-left: 5%;
+  margin-right: 5%;
 }
 </style>
